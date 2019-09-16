d0f222693c2224a0039c93002f4008efc9e0feb3
Takahiro Yano <speg03@gmail.com>
2019-09-16 16:57:55 +0900

    :children_crossing: Raise an error when invalid parameters were specified

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,3 +1,5 @@
+import pytest
+
 import nestargs
 from nestargs.meta import get_metadata
 
@@ -10,3 +12,10 @@ class TestOption:
 
         expected = {"nargs": 2, "help": "help for parameter"}
         assert get_metadata(some_function, "param", "arg_params") == expected
+
+    def test_option_with_invalid_parameter(self):
+        with pytest.raises(ValueError):
+
+            @nestargs.option("invalid", help="invalid parameter")
+            def some_function(param):
+                pass
